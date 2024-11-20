a36c339591f252fe3a10c5f879ee310e23018553
JimcostDev <rjak727@gmail.com>
2024-11-20 15:10:20 +0100

    evitar error de cors para el frontend

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 # Importaciones necesarias de FastAPI, HTTPException, 
 from fastapi import FastAPI, HTTPException
+from fastapi.middleware.cors import CORSMiddleware  
 from pathlib import Path
 
 
@@ -7,6 +8,15 @@ from pathlib import Path
 app = FastAPI(title="JimcostDev Football API",
     description="API para obtener información de equipos de fútbol",
     version="0.1.0",)
+
+# Configurar CORS para permitir solicitudes desde cualquier origen
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],  # Permite todos los orígenes, es decir, cualquier dominio puede hacer solicitudes
+    allow_credentials=True,
+    allow_methods=["*"],  # Permite todos los métodos HTTP (GET, POST, PUT, DELETE, etc.)
+    allow_headers=["*"],  # Permite todos los encabezados
+)
 
 # Función para cargar rutas dinámicamente
 def load_routes(app):
